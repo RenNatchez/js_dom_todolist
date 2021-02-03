83ed09ece75a11b901dad07b58ff53b4fd306df4
natchez <natchezzz@hotmail.com>
2021-02-03 17:26:42 +0100

    création de taches ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,43 @@
 // select btn envoyer
 let btnEnvoyer = document.querySelector('#envoyer')
-console.log(btnEnvoyer)
+// select input
+let input = document.querySelector('input')
+// select liste
+let liste = document.querySelector('.listTaches')
+// select tache
+let li = document.querySelectorAll('li')
+
+// ajout des taches
+    // ajout au click
+    let addTache = () => {
+        var li = document.createElement("li")
+        li.appendChild(document.createTextNode(input.value))
+        liste.appendChild(li)
+        input.value = ""
+    }
+    btnEnvoyer.addEventListener("click", addTache)
+
+    // ajout au enter
+    let addTacheEnter = (event) => {
+        if (event.which === 13) {
+            addTache()
+        }
+    }
+    input.addEventListener('keypress',addTacheEnter)
+
+
+
+//valider la taches
+let tacheOk = () => {
+    alert('test')
+}
+li.addEventListener("mouseover", tacheOk)
+
+
+let btnSup = document.createElement('button')
+btnSup.appendChild(document.createTextNode("lol"))
+li.appendChild(btnSup)
+btnSup.addEventListener("click", tacheOk)
+let suprimer = () => {
+    li.classList.add("none")
+}
